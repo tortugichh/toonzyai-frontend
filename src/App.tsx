3063547ec272e48bd9d767df4947dabac23fcd30
tortugichh <otesh0621@gmail.com>
2025-07-17 18:23:05 +0500

    id fix error

--- src/App.tsx
+++ src/App.tsx
@@ -124,7 +124,7 @@ function AppRoutes() {
           }
         />
         <Route
-          path="/stories/:id"
+          path="/stories/id"
           element={
             <ProtectedRoute>
               <StoryDetailPage />
